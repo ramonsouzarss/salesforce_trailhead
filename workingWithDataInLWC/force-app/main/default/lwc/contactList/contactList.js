c60be75395d2400d6d98deb951b45f533dae8006
Ramon Souza <ramonsouzasom@gmail.com>
2025-06-17 16:51:00 +0200

    showing errors with AuraHandledException

--- workingWithDataInLWC/force-app/main/default/lwc/contactList/contactList.js
+++ workingWithDataInLWC/force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRST_NAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Last Name', fieldName: LAST_NAME_FIELD.fieldApiName, type: 'text' },
@@ -12,4 +13,8 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+    get errors() {
+    return (this.accounts.error) ?
+        reduceErrors(this.accounts.error) : [];
+    }
 }
