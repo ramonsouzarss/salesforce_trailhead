83202e060b2f5a517f2937c9357bd60177524ec3
Ramon Souza <ramonsouzasom@gmail.com>
2025-06-19 16:06:37 +0200

    identation fixed

--- test-lwc/force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
+++ test-lwc/force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
@@ -41,18 +41,18 @@ describe('c-unit-test', () => {
         });
     });
 
-      it('displays unit status with input change event', () => {
-    const element = createElement('c-unit-test', {
-      is: UnitTest
-    });
-    document.body.appendChild(element);
-    const div = element.shadowRoot.querySelector('div');
-    // Trigger unit status input change
-    const inputElement = element.shadowRoot.querySelector('lightning-input');
-    inputElement.value = 7;
-    inputElement.dispatchEvent(new CustomEvent('change'));
-    return Promise.resolve().then(() => {
-      expect(div.textContent).toBe('Unit 7 alive!');
-    });
-  });  
+    it('displays unit status with input change event', () => {
+        const element = createElement('c-unit-test', {
+        is: UnitTest
+        });
+        document.body.appendChild(element);
+        const div = element.shadowRoot.querySelector('div');
+        // Trigger unit status input change
+        const inputElement = element.shadowRoot.querySelector('lightning-input');
+        inputElement.value = 7;
+        inputElement.dispatchEvent(new CustomEvent('change'));
+        return Promise.resolve().then(() => {
+        expect(div.textContent).toBe('Unit 7 alive!');
+        });
+    });  
 });
